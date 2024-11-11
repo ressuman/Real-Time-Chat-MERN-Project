2e8be7615b8effa8e70ef24e732a8b105d9ee1c9
Richard Essuman <ressuman001@gmail.com>
2024-11-11 14:01:35 +0000

    "Updated client code: changed index.html, package-lock.json, and package.json; added react-router-dom and updated App.jsx to use BrowserRouter and Routes; updated index.css with new styles for login and signup pages.".Add main application pages and routing with React Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/home";
+import Profile from "./pages/profile";
+import Login from "./pages/login";
+import Signup from "./pages/signup";
+import NotFound from "./pages/not-found";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Routes>
+          {/* Protected Routes */}
+          <Route
+            path="/"
+            element={
+              // <ProtectedRoute>
+              <Home />
+              //</ProtectedRoute>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              //<ProtectedRoute>
+              <Profile />
+              //</ProtectedRoute>
+            }
+          />
+
+          {/* Public Routes */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+
+          {/* Not Found Route */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/pages/signup/index.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+
+export default function Signup() {
+  const [user, setUser] = React.useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+
+  return (
+    <div className="container">
+      <div className="container-back-img"></div>
+      <div className="container-back-color"></div>
+      <div className="card">
+        <div className="card_title">
+          <h1>Create Account</h1>
+        </div>
+        <div className="form">
+          <form>
+            <div className="column">
+              <input
+                type="text"
+                placeholder="First Name"
+                value={user.firstName}
+                onChange={(e) =>
+                  setUser({ ...user, firstName: e.target.value })
+                }
+              />
+              <input
+                type="text"
+                placeholder="Last Name"
+                value={user.lastName}
+                onChange={(e) => setUser({ ...user, lastName: e.target.value })}
+              />
+            </div>
+            <input
+              type="email"
+              placeholder="Email"
+              value={user.email}
+              onChange={(e) => setUser({ ...user, email: e.target.value })}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              value={user.password}
+              onChange={(e) => setUser({ ...user, password: e.target.value })}
+            />
+            <button>Sign Up</button>
+          </form>
+        </div>
+        <div className="card_terms">
+          <span>
+            Already have an account?{""}
+            <a href="#gh">Login Here</a>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
